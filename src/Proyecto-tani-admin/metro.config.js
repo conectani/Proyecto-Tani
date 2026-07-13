618bbcf8c6547325c25406ec3496b57fea6a0dc1
Rodrigo Ricardo Mallqui Ordóñez <u22314039@utp.edu.pe>
2026-07-12 20:31:37 -0500

    fix(web): resolver problema de import.meta en Zustand v5 para Web

--- src/Proyecto-tani-admin/metro.config.js
+++ src/Proyecto-tani-admin/metro.config.js
@@ -5,4 +5,15 @@ const config = getDefaultConfig(__dirname);
 // Agregar soporte para reproducir videos locales .mp4 en la aplicación
 config.resolver.assetExts.push('mp4');
 
+// Resolver import.meta en Zustand v5 para Web
+config.resolver.resolveRequest = (context, moduleName, platform) => {
+  if (platform === 'web' && (moduleName === 'zustand' || moduleName.startsWith('zustand/'))) {
+    return {
+      filePath: require.resolve(moduleName),
+      type: 'sourceFile',
+    };
+  }
+  return context.resolveRequest(context, moduleName, platform);
+};
+
 module.exports = config;
