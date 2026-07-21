ca5f6d323f6e5f4d2eec806543ffdebc2b88234a
Rodrigo Ricardo Mallqui Ordóñez <u22314039@utp.edu.pe>
2026-07-21 13:20:45 -0500

    fix(router): configurar baseUrl /tani para subcarpeta en Expo Router web

--- src/Proyecto-tani/scripts/fix-web-scripts.js
+++ src/Proyecto-tani/scripts/fix-web-scripts.js
@@ -19,28 +19,18 @@ if (fs.existsSync(distDir)) {
   walkDir(distDir, (filePath) => {
     if (path.extname(filePath) === '.html') {
       let content = fs.readFileSync(filePath, 'utf8');
-      let modified = false;
-      if (content.includes('/_expo/static/js/web/')) {
-        content = content.replaceAll(
-          'src="/_expo/static/js/web/',
-          'src="/tani/_expo/static/js/web/'
-        ).replaceAll(
-          'href="/_expo/',
-          'href="/tani/_expo/'
-        ).replaceAll(
-          'src="/_expo/',
-          'src="/tani/_expo/'
-        );
-        modified = true;
-      }
-      if (!content.includes('type="module"')) {
+      if (content.includes('<script src="/tani/_expo/static/js/web/')) {
         content = content.replaceAll(
           '<script src="/tani/_expo/static/js/web/',
           '<script type="module" src="/tani/_expo/static/js/web/'
         );
-        modified = true;
-      }
-      if (modified) {
+        fs.writeFileSync(filePath, content, 'utf8');
+        console.log(`Corregido script tag en: ${path.relative(distDir, filePath)}`);
+      } else if (content.includes('<script src="/_expo/static/js/web/')) {
+        content = content.replaceAll(
+          '<script src="/_expo/static/js/web/',
+          '<script type="module" src="/_expo/static/js/web/'
+        );
         fs.writeFileSync(filePath, content, 'utf8');
         console.log(`Corregido script tag en: ${path.relative(distDir, filePath)}`);
       }
